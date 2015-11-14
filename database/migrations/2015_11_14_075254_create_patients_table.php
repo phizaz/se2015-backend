c5cb0cee5d0514402d43eb6f7d24da5371006df4
Teerapat <ham_smat@hotmail.com>
2015-11-14 19:54:31 +0700

    migration explanation

--- database/migrations/2015_11_14_075254_create_patients_table.php
+++ database/migrations/2015_11_14_075254_create_patients_table.php
@@ -15,7 +15,7 @@ class CreatePatientsTable extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('personal_id')->unique();
+            $table->string('personal_id', 13)->unique();
             $table->string('password', 60);
             $table->string('firstname');
             $table->string('lastname');
