d0c3c74b510b100f04c7f953ce377a35afb5d8f2
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-11-14 15:16:42 +0700

    model examples

---
+++ database/migrations/2015_11_14_075254_create_patients_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('personal_id')->unique();
+            $table->string('password', 60);
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('birthdate');
+            $table->string('address');
+            // gender: M, F
+            $table->string('gender', 1);
+            $table->string('nationality');
+            $table->string('religion')->nullable();
+            $table->string('bloodtype')->nullable();
+            // status: บอกว่าผู้ป่วยได้ตรวจเสร็จแล้ว และตอนนี้กำลังรอรับยาหรือเปล่า ?
+            $table->boolean('status')->default(0);
+            $table->string('remark')->nullable();
+            $table->integer('priority')->default(0);
+
+
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('patients');
+    }
+}
