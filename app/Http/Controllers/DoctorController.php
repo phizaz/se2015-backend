e4e48360025f250b38e4d7c0e953140ca1ae4b2d
zeleer <chlorococcum_unable@hotmail.com>
2015-11-19 11:42:00 +0700

    Doctor list doctor ทั้งหมดที่มีในระบบ ออกมา

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -11,14 +11,35 @@ use App\HospitalEmployee;
 class DoctorController extends Controller
 {
     public function doctor (){
-        $doctorName = [];
-        $doctorSurname = [];
+        $doctorFirstname = [];
+        $doctorLastname = [];
         $doctorId = [];
+        $doctorSpecialty = [];
+        $getDoctor = [];
+            
+        $doctors = HospitalEmployee::where('type','Doctor')->get();
 
-        while(){
-            if(HospitalEmployee::where('type','Doctor')->first()){
-                $doctor
-            }
+        // echo 'eiei<br>';
+
+        foreach ($doctors as $doctor) {
+            // echo 'ii';
+            // $doctorFirstname[] = $doctor->firstname;
+            // $doctorLastname[] = $doctor->lastname;
+            // $doctorId[] = $doctor->emp_id;
+            // $doctorSpecialty[] = $doctor->specialty;
+
+            // echo 'docro<br>';//$doctor->firstname;
+
+            $getDoctor[] = ['firstname' => $doctor->firstname,
+                            'lastname' => $doctor->lastname, 
+                            'id' => $doctor->emp_id, 
+                            'specialty' => $doctor->specialty];
         }
+
+        return response()->json([
+                "success" => true,
+                "data" => $getDoctor
+            ]);
+
     }
 }
