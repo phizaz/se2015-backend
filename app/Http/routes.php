23625d9973cb0583626448f585c270d93311c0ab
zeleer <chlorococcum_unable@hotmail.com>
2015-11-24 02:09:03 +0700

    ทำ function symptomReportCreate เเต่ UserController เเค่ลบ use User ออกไป อะมันซ้ำกัน

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -61,6 +61,8 @@ Route::post('/drug-record/update/{drug_id}','DoctorController@drugRecordUpdate')
 
 Route::post('/drug-record/delete/{drug_id}','DoctorController@drugRecordDelete');
 
+route::post('/symptom-report/create','DoctorController@symptomReportCreate');
+
 //-------MakeAppointmentController-----------
 
 Route::get('/find-options/doctor/{doctor_id}','MakeAppointmentController@getFreeSlotByDoctor');
---
+++ app/SymptomReport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SymptomReport extends Model
+{
+    //
+    protected $primaryKey = 'symptom_id';
+}
